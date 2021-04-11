e5de24618999e6f274d4b3572979117393f9d849
Anastasia Chuikova <orchkokos116@gmail.com>
2021-04-11 20:52:40 +0300

    fix PosixPath

--- galartus/settings.py
+++ galartus/settings.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
+    # Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -88,7 +89,7 @@ WSGI_APPLICATION = 'galartus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -137,4 +138,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
