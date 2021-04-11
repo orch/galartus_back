9510e23ab0823e4795a435fc29c6d9095c03d4c1
Anastasia Chuikova <orchkokos116@gmail.com>
2021-04-11 19:13:09 +0300

    Pictures bug

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories', views.CategoriesListCreateView.as_view()),
-    path('pictures', views.PicturesListCreateView.as_view()),
+    path('categories', views.CategoriesListView.as_view()),
+    path('categories/create', views.CategoriesCreateView.as_view()),
+    path('pictures', views.PicturesListView.as_view()),
+    path('pictures/create', views.PicturesCreateView.as_view()),
     path('categories/<int:pk>', views.CategoriesUpdateView.as_view()),
     path('pictures/<int:pk>', views.PicturesUpdateView.as_view()),
     path('categories/<int:pk>/delete', views.CategoriesDeleteView.as_view()),
--- main/views.py
+++ main/views.py
@@ -2,14 +2,28 @@ from django.http import HttpResponse
 from .models import Categories, Pictures
 from .serialize import CategoriesSerializer, PicturesSerializer
 from rest_framework import generics, renderers
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
 
 
 # categories
-class CategoriesListCreateView(generics.ListCreateAPIView):
+class CategoriesListView(generics.ListAPIView):
     queryset = Categories.objects.all()
     serializer_class = CategoriesSerializer
     filterset_fields = ['id', 'name']
-    renderer_classes = [renderers.JSONRenderer]
+
+
+class CategoriesCreateView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        serializer = CategoriesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return HttpResponse(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return HttpResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CategoriesUpdateView(generics.RetrieveUpdateAPIView):
@@ -23,11 +37,22 @@ class CategoriesDeleteView(generics.DestroyAPIView):
 
 
 # pictures
-class PicturesListCreateView(generics.ListCreateAPIView):
+class PicturesListView(generics.ListAPIView):
     queryset = Pictures.objects.all()
     serializer_class = PicturesSerializer
     filterset_fields = ['name', 'author', 'id', 'categories']
-    renderer_classes = [renderers.JSONRenderer]
+
+
+class PicturesCreateView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        serializer = PicturesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return HttpResponse(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return HttpResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PicturesUpdateView(generics.RetrieveUpdateAPIView):
